e0e992562cf87f7214828863cf56528091c391ad
VirulentKid <39715359+VirulentKid@users.noreply.github.com>
2020-03-16 16:34:03 -0400

    New classes templates
    added Member
    added MemberDatabase

---
+++ Member.py
@@ -0,0 +1,10 @@
+class Member:
+    def __init__(self, name, ID):
+        self.name = name
+        self.ID = ID
+
+    def get_name(self):
+        return self.name
+
+    def get_id(self):
+        return self.ID

d1702bcc6a86a1f58d2b351d6d3da1ea82ceefcd
VirulentKid <39715359+VirulentKid@users.noreply.github.com>
2020-03-22 19:41:49 -0400

    added classes
    added attendanceRecord, status, and time

--- MemberDatabase.py
+++ MemberDatabase.py
@@ -16,4 +16,25 @@ class MemberDatabase(object):
         else:
             return False
 
+    def update(self, member):
+        for i in self.database:
+            if i.get_id() == member.get_id():
+                self.database.remove(i)
+                self.database.append(member)
+                return True
+        return False
 
+    def retrieve(self, member_id):
+        for i in self.database:
+            if i.get_id() == member_id:
+                return i
+
+        print("This member is not in the database.")
+
+    def delete(self, member_id):
+        for i in self.database:
+            if i.get_id() == member_id():
+                self.database.remove(i)
+                return True
+
+        return False

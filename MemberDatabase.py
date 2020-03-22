844899601e2194a7ecc728770edbdf985826ddb1
VirulentKid <39715359+VirulentKid@users.noreply.github.com>
2020-03-22 18:38:22 -0400

    Update MemberDatabase.py
    Added is_present and add function

--- MemberDatabase.py
+++ MemberDatabase.py
@@ -1,2 +1,19 @@
 class MemberDatabase(object):
-    database = []
+    def __int__(self):
+        self.database = []
+
+    def is_present(self, member_id):
+        for i in self.database:
+            if i.get_id() == member_id:
+                return True
+            else:
+                return False
+
+    def add(self, member):
+        if not self.is_present(member.get_id):
+            self.database.append(member)
+            return True
+        else:
+            return False
+
+

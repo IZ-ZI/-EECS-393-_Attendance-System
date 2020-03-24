123bebbcc6b69af38ab19e5511c908cf60ef6934
VirulentKid <39715359+VirulentKid@users.noreply.github.com>
2020-03-23 23:44:26 -0400

    Update Activity.py
    added needed features for activity

--- Activity.py
+++ Activity.py
@@ -1,3 +1,24 @@
+import Time as Time
+
 class Activity:
-    def __init__(self):
-        return True
+    def __init__(self, name, start_time: Time, end_time: Time, location, priority):
+        self.name = name
+        self.start_time = start_time
+        self.end_time = end_time
+        self.location = location
+        self.priority = priority
+
+    def get_name(self) -> str:
+        return self.name
+
+    def get_start_time(self):
+        return self.start_time;
+
+    def get_end_time(self):
+        return self.end_time
+
+    def get_location(self):
+        return self.location
+
+    def get_priority(self):
+        return self.priority

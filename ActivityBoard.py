1cb39429926dd75c1a4826b4acc376e229e5a42c
VirulentKid <39715359+VirulentKid@users.noreply.github.com>
2020-03-24 01:02:10 -0400

    Create ActivityBoard.py
    this class is added for presenting all activities (temporarily, until the real database comes)

---
+++ ActivityBoard.py
@@ -0,0 +1,43 @@
+from Activity import Activity
+
+
+class ActivityBoard:
+    def __init__(self):
+        self.activities = []
+
+    def is_present(self, name):
+        for i in self.activities:
+            if i.get_name() == name:
+                return True
+            else:
+                return False
+
+    def add(self, activity: Activity):
+        if not self.is_present(activity.get_name()):
+            self.activities.append(activity)
+            return True
+        else:
+            return False
+
+    def update(self, activity: Activity):
+        for i in self.activities:
+            if i.get_name() == activity.get_name():
+                self.activities.remove(i)
+                self.activities.append(activity)
+                return True
+        return False
+
+    def retrieve(self, name):
+        for i in self.activities:
+            if i.get_name() == name:
+                return i
+
+        print("The member with the ID:", name, " is not in the database.")
+
+    def delete(self, name):
+        for i in self.activities:
+            if i.get_name() == name:
+                self.activities.remove(i)
+                return True
+
+        return False

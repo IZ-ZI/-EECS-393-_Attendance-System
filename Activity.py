2c6ec1627480ae4c2cccc80844cbd916083eb6d2
VirulentKid <39715359+VirulentKid@users.noreply.github.com>
2020-03-23 23:54:31 -0400

    Update Activity.py
    minor typo

--- Activity.py
+++ Activity.py
@@ -12,7 +12,7 @@ class Activity:
         return self.name
 
     def get_start_time(self):
-        return self.start_time;
+        return self.start_time
 
     def get_end_time(self):
         return self.end_time

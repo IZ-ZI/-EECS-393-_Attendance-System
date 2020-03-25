a39b6a16b2e7e00335dd8ae1f0584bbd241b6fa1
VirulentKid <39715359+VirulentKid@users.noreply.github.com>
2020-03-25 00:10:34 -0400

    Delete Driver.py

--- Driver.py
+++
@@ -1,8 +0,0 @@
-import MemberDatabase
-import Waitlist
-
-
-class Driver:
-    def __init__(self, member_database: MemberDatabase, waitlist: Waitlist):
-        self.member_database = member_database
-        self.waitlist = waitlist

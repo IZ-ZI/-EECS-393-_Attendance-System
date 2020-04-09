16b33a19315efa183fdf927cfac3b25be7ff8392
VirulentKid <terr7436@gmail.com>
2020-04-09 00:58:26 -0400

    Update FaceIdentification
    added a validation feature when setting the face id.

--- FaceIdentification.py
+++ FaceIdentification.py
@@ -1,4 +1,5 @@
 import face_recognition
+import os
 from ecapture import ecapture as ec
 
 
@@ -7,11 +8,22 @@ class FaceIdentification:
     def __init__(self):
         self.face_id = None
 
-    def set_face_id(self, name):
-        ec.capture(1, False, name + ".jpg")
-        self.face_id = face_recognition.load_image_file(name + ".jpg")
+    #return true if face id is successfully set
+    def set_face_id(self, member_id):
+        ec.capture(1, False, member_id + ".jpg")
+        self.face_id = face_recognition.load_image_file(member_id + ".jpg")
+        return self.test_face_is_set()
 
     def compare_to(self, image):
         this_encoding = face_recognition.face_encodings(self.face_id)[0]
         other_encoding = face_recognition.face_encodings(image)[0]
         return face_recognition.compare_faces([this_encoding], other_encoding)
+
+    def test_face_is_set(self):
+        ec.capture(1, False, "tester.jpg")
+        tester = face_recognition.load_image_file("tester.jpg")
+        this_encoding = face_recognition.face_encodings(self.face_id)[0]
+        tester_encoding = face_recognition.face_encodings(tester)[0]
+        result = face_recognition.compare_faces([this_encoding], tester_encoding)
+        os.remove("tester.jpg")
+        return result
--- face_id_test.py
+++ face_id_test.py
@@ -5,7 +5,7 @@ from ecapture import ecapture as ec
 test = FaceIdentification()
 
 ec.capture(1, False,"test.jpg")
-test.set_face_id("abc")
+print(test.set_face_id("abc"))
 image = face_recognition.load_image_file("test.jpg")
 
 result = test.compare_to(image)

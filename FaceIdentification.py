6ca8ec1490a60eafcba40843436cfcb6774bb980
VirulentKid <terr7436@gmail.com>
2020-04-09 01:05:51 -0400

    added face id feature to member

--- FaceIdentification.py
+++ FaceIdentification.py
@@ -9,7 +9,7 @@ class FaceIdentification:
         self.face_id = None
 
     #return true if face id is successfully set
-    def set_face_id(self, member_id):
+    def set_face_id(self, member_id) -> bool:
         ec.capture(1, False, member_id + ".jpg")
         self.face_id = face_recognition.load_image_file(member_id + ".jpg")
         return self.test_face_is_set()

e80ae1f67d1430c382f04bea8cf1fb4d07989b9a
VirulentKid <terr7436@gmail.com>
2020-04-10 19:14:33 -0400

    Update AttendanceRecord.py
    changed the implementation to take attendance

--- AttendanceRecord.py
+++ AttendanceRecord.py
@@ -1,10 +1,19 @@
+from Activity import Activity
+from datetime import datetime
+from Member import Member
+
 class AttendanceRecord:
-    def __init__(self, activity, status):
+    def __init__(self, activity: Activity, member: Member, status):
         self.activity = activity
+        self.member = member
         self.status = status
 
-    def get_arrival_time(self):
-        return self.time
+    def get_activity(self):
+        return self.activity
 
     def get_attendance_status(self):
         return self.status
+
+    def take_attendance(self):
+        arrival_time = datetime.now()
+        

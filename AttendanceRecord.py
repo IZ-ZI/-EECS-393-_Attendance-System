73ac658088932f7a6a8ce8331bc928e1b16ec1db
VirulentKid <terr7436@gmail.com>
2020-04-11 03:30:09 -0400

    Update attendancerecord
    now you can take attendance of an activity using attendance record and face id

--- AttendanceRecord.py
+++ AttendanceRecord.py
@@ -1,39 +1,42 @@
 from Activity import Activity
 from datetime import datetime
-from Member import Member
-from Status import Status
+from ecapture import ecapture as ec
+import face_recognition
+import os
 from FaceIdentification import FaceIdentification
 
 
 class AttendanceRecord:
-    def __init__(self, activity: Activity, member: Member):
+    def __init__(self, activity: Activity):
         self.activity = activity
-        self.member = member
+        self.status = ""
 
     def get_activity(self):
         return self.activity
 
-    def get_attendance_status(self):
+    def get_status(self):
         return self.status
 
-    def take_attendance(self):
+    def take_attendance(self, member):
         current_status = self.figured_status(datetime.now())
-        taken_face = FaceIdentification()
-        taken_face.set_face_id()
-
+        ec.capture(1, "photo taken", "taken_face.jpg")
+        taken_face = face_recognition.load_image_file("taken_face.jpg")
         if not member.get_face_id().compare_to(taken_face):
-            return false
+            os.remove("taken_face.jpg")
+            return True
         else:
             self.status = current_status
+            os.remove("taken_face.jpg")
+            return True
 
     def figured_status(self, time: datetime):
-        if time <= activity.get_start_time():
-            return Status.on_time
+        if time <= self.activity.get_start_time():
+            return "on_time"
 
-        elif time <= activity.get_end_time():
-            return Status.late
+        elif time <= self.activity.get_end_time():
+            return "late"
 
         else:
-            return Status.absent
+            return "absent"
 
 

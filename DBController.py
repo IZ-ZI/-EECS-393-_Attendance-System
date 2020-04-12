a12d51968b3a36f6862e4dc1577e159c81391b5c
wz9314 <61978931+wz9314@users.noreply.github.com>
2020-04-12 02:10:58 -0400

    add activity database

--- DBController.py
+++ DBController.py
@@ -5,9 +5,10 @@ import sendEmail as se
 
 class DBController:
 
-    def __init__(self,collection_member, collection_admin):
+    def __init__(self, collection_member, collection_admin, collection_activity):
         self.collection_member = collection_member
         self.collection_admin = collection_admin
+        self.collection_activity = collection_activity
 
     def member_is_present(self, member_id):
         member = self.collection_member.find_one({"_id": member_id})
@@ -16,7 +17,7 @@ class DBController:
     def add_member(self, member):
         if not self.member_is_present(member.get_id()):
             post = {"_id": member.get_id(), "name": member.get_name(), "email_address": member.get_email_address(),
-                    "password": member.get_password(), "clubs": []}
+                    "password": member.get_password(), "face id": member.get_face_id(), "clubs": [], "activities": []}
             self.collection_member.insert_one(post)
             return True
         else:
@@ -101,11 +102,6 @@ class DBController:
 
 
 
-
-
-
-
-
     def admin_is_present(self, admin_id):
         admin = self.collection_admin.find_one({"_id": admin_id})
         return admin is not None
@@ -114,7 +110,7 @@ class DBController:
         if not self.admin_is_present(admin.get_organization_id()):
             post = {"_id": admin.get_organization_id(), "name": admin.get_organization_name(),
                     "email_address": admin.get_email_address(),
-                    "password": admin.get_password(), "added_members": [], "pending_members": []}
+                    "password": admin.get_password(), "added_members": [], "pending_members": [], "activities": []}
             self.collection_admin.insert_one(post)
             return True
         else:
@@ -212,3 +208,112 @@ class DBController:
         se.send_email('attsystem393@gmail.com', 'eecs_393',
                       member_email, admin_name, admin_id, False)
         return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def activity_is_present(self, activity_id):
+        activity = self.collection_activity.find_one({"_id": activity_id})
+        return activity is not None
+
+    def add_activity(self, activity):
+        if not self.activity_is_present(activity.get_id()):
+            post = {"_id": activity.get_id(), "name": activity.get_name(), "start_time": activity.get_start_time(),
+                    "end_time": activity.get_end_time(), "location": activity.get_location()}
+            self.collection_activity.insert_one(post)
+            return True
+        else:
+            return False
+
+
+    def update_activity(self, activity):
+        if self.admin_is_present(activity.get_id()):
+            self.collection_activity.update_one({"_id": activity.get_id()}, {'$set':
+                {
+                    "name": activity.get_name(), "start_time": activity.get_start_time(),
+                    "end_time": activity.get_end_time(), "location": activity.get_location()
+                }
+            })
+            return True
+        else:
+            return False
+
+    # return a dictionary in a form like "_id": admin.get_organization_id(), "name": admin.get_organization_name(),
+    # "email_address": admin.get_email_address(),"password": admin.get_password(), "clubs": []
+    def retrieve_activity(self, activity_id):
+        return self.collection_activity.find_one({"_id": activity_id})
+
+    def delete_activity(self, activity_id):
+        if self.activity_is_present(activity_id):
+            self.collection_activity.delete_one({"_id": activity_id})
+            return True
+        else:
+            return False
+
+
+
+    def add_activity_to_member(self, activity_id, member_id):
+        if self.member_is_present(member_id) and activity_id not in self.member_activities(member_id):
+            self.collection_member.update_one(
+                {"_id": member_id},
+                {'$push': {"activities": activity_id}}
+            )
+            return True
+        else:
+            return False
+
+    def remove_activity_from_member(self, activity_id, member_id):
+        if self.member_is_present(member_id) and activity_id in self.member_activities(member_id):
+            self.collection_member.update_one(
+                {"_id": member_id},
+                {'$pull': {"activities": activity_id}}
+            )
+            return True
+        else:
+            return False
+
+    def add_activity_to_admin(self, activity_id, admin_id):
+        if self.member_is_present(admin_id) and activity_id not in self.admin_activities(admin_id):
+            self.collection_admin.update_one(
+                {"_id": admin_id},
+                {'$push': {"activities": activity_id}}
+            )
+            return True
+        else:
+            return False
+
+    def remove_activity_from_admin(self, activity_id, admin_id):
+        if self.admin_is_present(admin_id) and activity_id in self.admin_activities(admin_id):
+            self.collection_admin.update_one(
+                {"_id": admin_id},
+                {'$pull': {"activities": activity_id}}
+            )
+            return True
+        else:
+            return False
+
+    def member_activities(self, member_id):
+        if self.member_is_present(member_id):
+            cursor = self.collection_member.find_one({"_id": member_id})
+            return cursor["activities"]
+        else:
+            return None
+
+    def admin_activities(self, admin_id):
+        if self.admin_is_present(admin_id):
+            cursor = self.collection_admin.find_one({"_id": admin_id})
+            return cursor["activities"]
+        else:
+            return None

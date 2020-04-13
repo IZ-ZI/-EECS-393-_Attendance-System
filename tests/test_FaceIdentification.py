4391bc33c58db904ef40858ef6c657806aa09ed1
VirulentKid <terr7436@gmail.com>
2020-04-13 03:11:35 -0400

    Added test for Face Identification
    now coverage report can be generated

--- tests/test_FaceIdentification.py
+++ tests/test_FaceIdentification.py
@@ -1,13 +1,15 @@
 from unittest import TestCase
 from FaceIdentification import FaceIdentification as fi
 import face_recognition
+import os
+
 
 class TestFaceIdentification(TestCase):
 
-    photo1 = face_recognition.load_image_file("ha.png")
-    photo2 = face_recognition.load_image_file("y_ha.png")
-    photo3 = face_recognition.load_image_file("trump.png")
-    photo4 = face_recognition.load_image_file("cwru.png")
+    photo1 = face_recognition.load_image_file(os.path.abspath("ha.png"))
+    photo2 = face_recognition.load_image_file(os.path.abspath("y_ha.png"))
+    photo3 = face_recognition.load_image_file(os.path.abspath("trump.png"))
+    photo4 = face_recognition.load_image_file(os.path.abspath("cwru.png"))
     encoding1 = face_recognition.face_encodings(photo1)[0].tostring()
     encoding2 = face_recognition.face_encodings(photo2)[0].tostring()
     encoding3 = face_recognition.face_encodings(photo3)[0].tostring()

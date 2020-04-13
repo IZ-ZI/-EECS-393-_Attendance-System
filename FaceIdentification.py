caf36b1de535cc2a34d4c8ee7aec8e1d17bfa032
VirulentKid <terr7436@gmail.com>
2020-04-13 02:36:18 -0400

    Added FaceIdentificaiton test and fixed bugs in that class

--- FaceIdentification.py
+++ FaceIdentification.py
@@ -1,11 +1,14 @@
 import face_recognition
-
+import numpy
 
 class FaceIdentification:
 
     @staticmethod
     def compare_to(encoding1, encoding2):
-        return face_recognition.compare_faces([encoding1], encoding2)
+        encoding_1 = numpy.fromstring(encoding1)
+        encoding_2 = numpy.fromstring(encoding2)
+        result = face_recognition.compare_faces([encoding_1], encoding_2)
+        return result[0]
 
     @staticmethod
     def encoding_from_photo(photo):
--- tests/test_FaceIdentification.py
+++ tests/test_FaceIdentification.py
@@ -1,9 +1,22 @@
 from unittest import TestCase
-
+from FaceIdentification import FaceIdentification as fi
+import face_recognition
 
 class TestFaceIdentification(TestCase):
+
+    photo1 = face_recognition.load_image_file("ha.png")
+    photo2 = face_recognition.load_image_file("y_ha.png")
+    photo3 = face_recognition.load_image_file("trump.png")
+    photo4 = face_recognition.load_image_file("cwru.png")
+    encoding1 = face_recognition.face_encodings(photo1)[0].tostring()
+    encoding2 = face_recognition.face_encodings(photo2)[0].tostring()
+    encoding3 = face_recognition.face_encodings(photo3)[0].tostring()
+
     def test_compare_to(self):
-        self.fail()
+        self.assertTrue(fi.compare_to(self.encoding1, self.encoding2))
+        self.assertFalse(fi.compare_to(self.encoding1, self.encoding3))
 
     def test_encoding_from_photo(self):
-        self.fail()
+        encoding = fi.encoding_from_photo(self.photo4)
+        self.assertTrue(encoding == None)
+        self.assertTrue(self.encoding1 == fi.encoding_from_photo(self.photo1))
